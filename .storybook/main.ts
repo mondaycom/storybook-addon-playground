52d689b73475124145161ef72649e1aabb953636
Yossi Saadi <yossisa@monday.com>
2023-10-01 23:42:49 +0300

    chore: remove unneeded addons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,10 +2,18 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
     "./local-preset.js",
+    {
+      name: "@storybook/addon-essentials",
+      options: {
+        actions: false,
+        backgrounds: false,
+        highlight: false,
+        measure: false,
+        outline: false,
+        toolbars: false,
+      },
+    },
   ],
   framework: {
     name: "@storybook/react-vite",
