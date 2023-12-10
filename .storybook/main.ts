b13eee0f742a314a738db67a6ad310509e4474b0
Yossi Saadi <yossisa@monday.com>
2023-12-10 18:32:47 +0200

    docs: example storybook to show usage + to test the addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,26 +1,24 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["./**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "./local-preset.js",
+    "../dist/manager.js",
     {
       name: "@storybook/addon-essentials",
       options: {
         actions: false,
-        backgrounds: false,
+        controls: false,
         highlight: false,
         measure: false,
         outline: false,
         toolbars: false,
+        viewport: false,
       },
     },
   ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
-  },
-  docs: {
-    autodocs: "tag",
   },
 };
 export default config;
