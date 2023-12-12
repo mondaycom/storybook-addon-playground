b1026c21d58ad61436e9aeb39e239372b9186cf2
Yossi Saadi <yossisa@monday.com>
2023-12-12 14:47:44 +0200

    feat: remove lz-string for now, add postcss support instead of scss support

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import typescript from "rollup-plugin-typescript2";
-import scss from "rollup-plugin-scss";
+import postcss from "rollup-plugin-postcss";
 
 export default {
   input: ["src/index.ts", "src/manager.ts"],
@@ -20,6 +20,6 @@ export default {
     commonjs(),
     json(),
     typescript({ useTsconfigDeclarationDir: true }),
-    scss(),
+    postcss(),
   ],
 };
