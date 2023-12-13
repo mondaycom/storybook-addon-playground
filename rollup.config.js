bbfce17a3e8e0b2adf5f7716a3efda0b88f64e3d
Yossi Saadi <yossisa@monday.com>
2023-12-13 14:32:43 +0200

    refactor: make config more clear and straightforward, fix CR comments

--- rollup.config.js
+++ rollup.config.js
@@ -3,44 +3,18 @@ import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
-import fs from "fs";
 
-function loadEntries() {
-  const packageJson = fs.readFileSync("./package.json", "utf8");
-  const parsed = JSON.parse(packageJson);
-  return parsed.bundler || {};
-}
+const exportEntry = "src/index.ts";
+const managerEntry = "src/manager.ts";
 
-const { exportEntries = [], managerEntries = [] } = loadEntries();
-
-function generateConfig(entries, generateDts = false) {
-  return {
-    external: [/node_modules/],
-    plugins: [
-      resolve(),
-      commonjs(),
-      json(),
-      typescript({
-        tsconfigOverride: {
-          compilerOptions: {
-            declaration: generateDts,
-            declarationMap: generateDts,
-          },
-        },
-      }),
-      postcss(),
-    ],
-    input: entries,
-    output: {
-      dir: "dist",
-      format: "esm",
-      sourcemap: true,
-      preserveModules: true,
-    },
-  };
-}
-
-export default [
-  generateConfig(exportEntries, true),
-  generateConfig(managerEntries),
-];
+export default {
+  external: [/node_modules/],
+  plugins: [resolve(), commonjs(), json(), typescript(), postcss()],
+  input: [exportEntry, managerEntry],
+  output: {
+    dir: "dist",
+    format: "esm",
+    sourcemap: true,
+    preserveModules: true,
+  },
+};
