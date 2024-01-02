215006280f68383ea40192bedaed795bfbaa6ac2
Yossi Saadi <yossisa@monday.com>
2024-01-02 11:45:11 +0200

    test: add vitest infra for the addon (#15)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   build: {
     sourcemap: true,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    include: ["**/__tests__/**/*.ts", "**/__tests__/**/*.tsx"],
+    clearMocks: true,
+    typecheck: {
+      enabled: true,
+    },
+    passWithNoTests: true,
   },
 });
