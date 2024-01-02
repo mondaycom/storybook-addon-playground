4cbb4021e653e4e175db34a14bace8fddf459a93
Yossi Saadi <yossisa@monday.com>
2024-01-02 19:05:21 +0200

    test: mock @storybook/components component IconButton for failing with @storybook/theming

---
+++ __mocks__/@storybook/components.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface IconButtonProps {
+  onClick?: () => void;
+  disabled?: boolean;
+  className?: string;
+}
+
+export * from "@storybook/components";
+
+export const IconButton: React.FC<React.PropsWithChildren<IconButtonProps>> = ({
+  onClick,
+  disabled,
+  className,
+  children,
+}) => (
+  <button
+    type="button"
+    disabled={disabled}
+    onClick={onClick}
+    className={className}
+  >
+    {children}
+  </button>
+);
--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -2,6 +2,8 @@ import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
 
+vi.mock("@storybook/components");
+
 afterEach(() => {
   cleanup();
 });
