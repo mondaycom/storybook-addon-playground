4952c031534b6825a05af2f2fe84010effd3821e
Yossi Saadi <yossisa@monday.com>
2024-01-08 20:00:33 +0200

    refactor: manage the playground renderer based on events instead of args

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as usePlaygroundArgs } from "./usePlaygroundArgs";
 export { default as useToolbarActions } from "./useToolbarActions";
 export { default as useCopyToClipboard } from "./useCopyToClipboard";
+export { default as usePlaygroundRendererCode } from "./usePlaygroundRendererCode";
