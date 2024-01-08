c298ade623739f67608f7014e4745ecb8f7997e0
Yossi Saadi <yossisa@monday.com>
2024-01-08 17:04:16 +0200

    docs: add VibeNext to addon's local storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,13 @@
 import type { Preview } from "@storybook/react";
 import * as VibeComponents from "monday-ui-react-core";
+import * as VibeNext from "monday-ui-react-core/next";
 import * as VibeIcons from "monday-ui-react-core/icons";
 
 const preview: Preview = {
   parameters: {
     playground: {
       storyId: "playground",
-      components: { ...VibeComponents, VibeIcons },
+      components: { ...VibeComponents, VibeIcons, VibeNext },
       editorTheme: "light",
     },
   },
