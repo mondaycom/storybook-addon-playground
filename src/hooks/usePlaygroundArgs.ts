4bbbb1a8814169b32926df2e168474c4fa509fd8
Yossi Saadi <yossisa@monday.com>
2024-01-08 17:36:10 +0200

    refactor: update to story args now relies on effect

--- src/hooks/usePlaygroundArgs.ts
+++ src/hooks/usePlaygroundArgs.ts
@@ -1,6 +1,5 @@
-import { useAddonState, useArgs } from "@storybook/manager-api";
-import { useCallback, useMemo } from "react";
-import { debounce } from "lodash-es";
+import { useAddonState } from "@storybook/manager-api";
+import { useCallback } from "react";
 import { PlaygroundArgs, PlaygroundState } from "@/types";
 import { DEFAULT_ADDON_STATE, PANEL_ID } from "@/consts";
 
@@ -9,28 +8,20 @@ const usePlaygroundArgs = (): PlaygroundArgs => {
     PANEL_ID,
     DEFAULT_ADDON_STATE
   );
-  const [_args, updateArgs] = useArgs();
   const { code, selectedTab } = state;
-
-  const debouncedUpdateArgs = useMemo(
-    () => debounce(updateArgs, 300),
-    [updateArgs]
-  );
 
   const updateCode = useCallback(
     (newCode: string) => {
       const updatedCode = { ...code, [selectedTab]: newCode };
       setState((state) => ({ ...state, code: updatedCode }));
-      debouncedUpdateArgs({ code: updatedCode });
     },
-    [code, selectedTab, setState, debouncedUpdateArgs]
+    [code, selectedTab, setState]
   );
 
   const resetCode = useCallback(() => {
     const updatedCode = { ...code, [selectedTab]: "" };
     setState((state) => ({ ...state, code: updatedCode }));
-    debouncedUpdateArgs({ code: updatedCode });
-  }, [code, selectedTab, setState, debouncedUpdateArgs]);
+  }, [code, selectedTab, setState]);
 
   return { updateCode, resetCode };
 };
