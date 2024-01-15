c6e740a5a89dddb592b49e9af06c7bc66777a8e1
Yossi Saadi <yossisa@monday.com>
2024-01-15 21:41:23 +0200

    fix: cannot use LivePreview on a storybook decorator as it uses useState from react and not from preview-api

--- src/components/PlaygroundPreview/PlaygroundPreview.tsx
+++ src/components/PlaygroundPreview/PlaygroundPreview.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 import { Code } from "@/types";
-import { LivePreview, withLive } from "react-live";
+import { withLive } from "react-live";
 import ErrorState from "@/components/PlaygroundPreview/ErrorState";
 
 interface PlaygroundPreviewProps {
   live?: {
     // live injects more, currently unneeded, props
     error?: string;
+    element?: React.ComponentType;
   };
   css?: Code["css"];
 }
 
-const PlaygroundPreview: React.FC<PlaygroundPreviewProps> = ({ live, css }) => (
-  <>
-    {css && <style>{css}</style>}
-    {live.error ? <ErrorState error={live.error} /> : <LivePreview />}
-  </>
-);
+const PlaygroundPreview: React.FC<PlaygroundPreviewProps> = ({
+  live = {},
+  css,
+}) => {
+  const { error, element: Element } = live;
+  return (
+    <>
+      {css && <style>{css}</style>}
+      {error ? <ErrorState error={error} /> : Element && <Element />}
+    </>
+  );
+};
 export default withLive(PlaygroundPreview);
