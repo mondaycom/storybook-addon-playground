f75423bfdb0be7079cc79d30fc3e9eddb48c8f3a
Yossi Saadi <yossisa@monday.com>
2024-05-09 16:36:49 +0300

    feat(icons): add new custom icons

---
+++ src/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as Beaker } from "./Beaker";
+export { default as Copy } from "./Copy";
+export { default as Edit } from "./Edit";
+export { default as Reset } from "./Reset";
+export { default as Share } from "./Share";
